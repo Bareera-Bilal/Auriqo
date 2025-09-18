0cbf7c0e18b595febf52c82eb2d57be64e5cec8b
Bareera Bilal <bareerabilal03@gmail.com>
2025-09-18 09:45:29 +0530

    Add files via upload

--- Auriqo-Web-Api-Backend/Program.cs
+++ Auriqo-Web-Api-Backend/Program.cs
@@ -1,41 +1,62 @@
+
+using Microsoft.EntityFrameworkCore;
+using Auriqo_Web_Api_Backend.Interfaces;
+using Auriqo_Web_Api_Backend.Services;
+using Auriqo;
+using Auriqo_Web_Api_Backend.Models.JunctionModels;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddControllers();
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SqlDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("cloud")));
+
+// cors policy for allowing frontend to send request on this server 
+
+builder.Services.AddCors(Options => {Options.AddPolicy("AllowFrontend", policy => 
+policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod().AllowCredentials());});
+
+// register or configure the  options needed by EmailService having type of EmailSettings present in P0 classlibarary   
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+
+var cloudinaryUri = builder.Configuration["Cloudinary:URI"] ?? throw new InvalidOperationException("Cloudinary url not set !");
+var SecretKey = builder.Configuration["Jwt:SecretKey"] ?? throw new InvalidOperationException(" Secret Key not set !");
+
+
+
+// not regular you have to use lambda expression 
+builder.Services.AddSingleton<ICloudinaryService>(_ => new CloudinaryService(cloudinaryUri));
+builder.Services.AddSingleton<ITokenService>(_ => new TokenService(SecretKey));
+
+
+// regular as usual   // but complexity behind
+builder.Services.AddSingleton<IMailService, EmailService>();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+app.UseCors("AllowFrontend"); 
+
+// app.UseAuthorization(); 
+
+app.MapControllers();
+
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
