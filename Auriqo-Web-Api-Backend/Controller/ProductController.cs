80f94de71f98b246b31bde9727ffd96bc67e7a36
Bareera Bilal <bareerabilal03@gmail.com>
2025-09-19 16:11:09 +0530

    Add files via upload

--- Auriqo-Web-Api-Backend/Controller/ProductController.cs
+++ Auriqo-Web-Api-Backend/Controller/ProductController.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using Auriqo_Web_Api_Backend.Models.DomainModels;
 using Microsoft.AspNetCore.Mvc;
@@ -48,16 +50,20 @@ namespace Auriqo_Web_Api_Backend.Controllers
 
         [HttpGet("archive")]
 
-        public async Task<IActionResult> ArchiveProduct(Guid productId){
-            
+        public async Task<IActionResult> ArchiveProduct(Guid productId)
+        {
+
             try
             {
                 var product = await dbContext.Products.FindAsync(productId);
-                if(product == null){
-                    return NotFound(new{message="PRODUCT NOT FIND", productId});
+
+                if (product == null)
+                {
+                    return NotFound(new { message = "PRODUCT NOT FIND", productId });
                 }
 
-                if(product.IsArchived == false && product.IsAvailable == true){
+                if (product.IsArchived == false && product.IsAvailable == true)
+                {
                     product.IsArchived = true;
                     product.IsAvailable = false;
                     product.UpdatedAt = DateTime.Now;
@@ -65,23 +71,179 @@ namespace Auriqo_Web_Api_Backend.Controllers
 
                 }
 
-                else{
-                    return BadRequest(new{message="PRODUCT IS ALREADY IN ARCHIVE"});
+                else
+                {
+
+                    return BadRequest(new { message = "PRODUCT IS ALREADY IN ARCHIVE" });
                 }
+
+                return Ok(new
+                {
+                    message = "PRODUCT ARCHIVED!"
+                });
             }
             catch (System.Exception)
             {
-                
+
                 throw;
             }
         }
 
 
+        [HttpGet("unarchive")]
+
+        public async Task<IActionResult> UnArchiveProduct(Guid productId)
+        {
+            try
+            {
+                var product = await dbContext.Products.FindAsync(productId);
+
+                if (product == null)
+                {
+                    return NotFound(new { message = "PRODUCT NOT FOUND" });
+                }
+
+                if (product.IsArchived == true && product.IsAvailable == false)
+                {
+                    product.IsArchived = false;
+                    product.IsAvailable = true;
+                    product.UpdatedAt = DateTime.Now;
+                    await dbContext.SaveChangesAsync();
+                }
+
+                else
+                {
+                    return BadRequest(new { message = "PRODUCT IS ALREADY IN UNARCHIVED LIST" });
+                }
+
+                return Ok(new
+                {
+                    message = "PRODUCT UNARCHIVED",
+                    payload = product
+                });
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
 
 
 
+        [HttpGet("delete")]
+
+        public async Task<IActionResult> DeleteProduct(Guid productId)
+        {
+
+            try
+            {
+                var product = await dbContext.Products.FindAsync(productId);
+
+                if (product == null)
+                {
+                    return NotFound(new { message = "PRODUCT NOT FOUND", productId });
+                }
+
+                if (product.IsArchived == true && product.IsAvailable == false)
+                {
+                    product.IsArchived = false;
+                    product.UpdatedAt = DateTime.Now;
+
+                    await dbContext.SaveChangesAsync();
+                }
+
+                else
+                {
+                    return BadRequest(new { message = "PRODUCT IS SHIFTED TO RECYCLE BIN" });
+                }
+
+                return Ok();
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
 
 
+        [HttpPut("update")]
+
+        public async Task<IActionResult> UpdateProduct(Guid productId, Product product)
+        {
+            try
+            {
+                var existingproduct = await dbContext.Products.FindAsync(productId);
+
+                if (existingproduct == null)
+                {
+                    return NotFound(new { message = "PRODUCT NOT FOUND" });
+
+                }
+
+                existingproduct.ProductName = product.ProductName;
+                existingproduct.ProductDescription = product.ProductDescription;
+                existingproduct.ProductImage = product.ProductImage;
+                existingproduct.ProductStock = product.ProductStock;
+                existingproduct.ProductPrice = product.ProductPrice;
+                existingproduct.Size = product.Size;
+                existingproduct.Color = product.Color;
+                existingproduct.Weight = product.Weight;
+                existingproduct.Category = product.Category;
+                existingproduct.UpdatedAt = DateTime.Now;
+
+                await dbContext.SaveChangesAsync();
+                return Ok(new { message = "PRODUCT UPDATED SUCCESSFULLY", payload = existingproduct });
+
+
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+
+        [HttpGet("getAll")]
+        public async Task<IActionResult> GetProducts()
+        {
+            try
+            {
+                var products = await dbContext.Products.Where(p => p.IsAvailable == true).ToListAsync();
+
+                return Ok(new { message = $"{products.Count} PRODUCTS ARE FOUND", payload = products });
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        [HttpGet("getbyId")]
+
+        public async Task<IActionResult> GetProductById(Guid productId)
+        {
+            try
+            {
+                var product = await dbContext.Products.FindAsync(productId);
+                return Ok(new { message = " 1 PRODUCT FOUND", payload = product });
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+
+        }
 
 
 
